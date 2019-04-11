9be56e6600a059fe23e2de4726409b29a8a06bbe
Pooc <pooc95@163.com>
2019-04-11 16:21:23 +0800

    6-3 商品详情页评价列表开发

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -36,8 +36,8 @@
             <p class="text">{{food.info}}</p>
           </div>
           <split></split>
-<!--          <div class="rating">-->
-<!--            <h1 class="title">商品评价</h1>-->
+          <div class="rating">
+            <h1 class="title">商品评价</h1>
 <!--            <rating-select-->
 <!--              @select="onSelect"-->
 <!--              @toggle="onToggle"-->
@@ -46,26 +46,26 @@
 <!--              :desc="desc"-->
 <!--              :ratings="ratings">-->
 <!--            </rating-select>-->
-<!--            <div class="rating-wrapper">-->
-<!--              <ul v-show="computedRatings && computedRatings.length">-->
-<!--                <li-->
-<!--                  v-for="(rating,index) in computedRatings"-->
-<!--                  class="rating-item border-bottom-1px"-->
-<!--                  :key="index"-->
-<!--                >-->
-<!--                  <div class="user">-->
-<!--                    <span class="name">{{rating.username}}</span>-->
-<!--                    <img class="avatar" width="12" height="12" :src="rating.avatar">-->
-<!--                  </div>-->
-<!--                  <div class="time">{{format(rating.rateTime)}}</div>-->
-<!--                  <p class="text">-->
-<!--                    <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}-->
-<!--                  </p>-->
-<!--                </li>-->
-<!--              </ul>-->
-<!--              <div class="no-rating" v-show="!computedRatings || !computedRatings.length">暂无评价</div>-->
-<!--            </div>-->
-<!--          </div>-->
+            <div class="rating-wrapper">
+              <ul v-show="ratings && ratings.length">
+                <li
+                  v-for="(rating,index) in ratings"
+                  class="rating-item border-bottom-1px"
+                  :key="index"
+                >
+                  <div class="user">
+                    <span class="name">{{rating.username}}</span>
+                    <img class="avatar" width="12" height="12" :src="rating.avatar">
+                  </div>
+                  <div class="time">{{format(rating.rateTime)}}</div>
+                  <p class="text">
+                    <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
+                  </p>
+                </li>
+              </ul>
+              <div class="no-rating" v-show="!ratings || !ratings.length">暂无评价</div>
+            </div>
+          </div>
         </div>
       </cube-scroll>
     </div>
@@ -73,7 +73,7 @@
 </template>
 
 <script type="text/ecmascript-6">
-  // import moment from 'moment'
+  import moment from 'moment'
   import CartControl from 'components/cart-control/cart-control'
   // import RatingSelect from 'components/rating-select/rating-select'
   import Split from 'components/split/split'
@@ -101,11 +101,11 @@
     //     }
     //   }
     // },
-    // computed: {
-    //   ratings() {
-    //     return this.food.ratings
-    //   }
-    // },
+    computed: {
+      ratings() {
+        return this.food.ratings
+      }
+    },
     created() {
       this.$on(EVENT_SHOW, () => {
         this.$nextTick(() => {
@@ -123,10 +123,10 @@
       },
       addFood(target) {
         this.$emit(EVENT_ADD, target)
+      },
+      format(time) {
+        return moment(time).format('YYYY-MM-DD hh:mm')
       }
-    //   format(time) {
-    //     return moment(time).format('YYYY-MM-DD hh:mm')
-    //   }
     },
     components: {
       CartControl,
